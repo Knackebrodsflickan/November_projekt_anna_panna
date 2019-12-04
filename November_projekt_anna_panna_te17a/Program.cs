1996432adae550b09888952ea8a023815f6784bd
Knackebrodsflickan <54945141+Knackebrodsflickan@users.noreply.github.com>
2019-12-04 08:24:38 +0100

    ye

--- November_projekt_anna_panna_te17a/Program.cs
+++ November_projekt_anna_panna_te17a/Program.cs
@@ -9,15 +9,15 @@ namespace November_projekt_anna_panna_te17a
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // lägg till
         {
             Console.WriteLine("Welcome to Budget Mortal Wombat! The game where you suprisingly dont fight wombats! ");
-            Console.ReadLine();
-            Console.WriteLine("What would you like to name your Hero?"); // change readline to readkey memmo! 
+            Console.ReadKey();
+            Console.WriteLine("What would you like to name your Hero?"); 
             string MainName = Console.ReadLine(); //keep
 
             Console.WriteLine("Your Hero´s name is: " + MainName + "!");
-            Console.ReadLine();
+            Console.ReadKey();
 
             Hero MainHero = new Hero();
 
@@ -33,7 +33,7 @@ namespace November_projekt_anna_panna_te17a
             if(ReadEnemyName != "Farquaad" && ReadEnemyName != "The Fairy Godmother" && ReadEnemyName != "Prince Charming")
             {
                 Console.WriteLine("Uh oh! that is not one of your enemies - if you feel like fighting with someone not on this list try reddit :)");
-                Console.ReadLine();
+                Console.ReadKey();
             }
             else if(ReadEnemyName == "Farquaad" || ReadEnemyName == "The Fairy Godmother" || ReadEnemyName == "Prince Charming")
             {
